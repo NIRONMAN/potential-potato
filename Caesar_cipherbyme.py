29b603206d7c158e5c0e5019c8a97d9352c63291
NIRONMAN <108979457+NIRONMAN@users.noreply.github.com>
2022-07-24 15:50:19 +0530

    Create Caesar_cipherbyme.py

---
+++ Caesar_cipherbyme.py
@@ -0,0 +1,34 @@
+from art import logo
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a=True
+while (a):
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    def caesar(start_text, shift_amount, cipher_direction):
+      end_text = ""
+      if cipher_direction == "decode":
+        shift_amount *= -1
+      for char in start_text:
+        if char not in alphabet:
+          end_text+=char 
+          continue
+
+        position = alphabet.index(char)
+        new_position = (position + shift_amount)%26
+        end_text += alphabet[new_position]
+        
+      print(f"Here's the {cipher_direction}d result: {end_text}")
+   
+    
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+
+    p=input("Do you want to restart the cipher program?\n").lower()
+    if p=="yes":
+        a=True
+    elif p=="no":
+        print("Goodbye")
+        a=False
